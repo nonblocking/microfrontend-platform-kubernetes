6f6b34b6672a7c8f1980dc7f1fda065159706327
Jürgen Kofler <juergen.kofler@nonblocking.at>
2023-07-09 13:28:52 +0200

    grafana dashboard improved

--- k3d/setup-monitoring.sh
+++ k3d/setup-monitoring.sh
@@ -8,7 +8,6 @@ source $DIRECTORY/set-env.sh
 echo "Deploying Prometheus Operator"
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 kubectl apply -f - <<EOF
